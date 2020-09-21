b7418fdfa0d0f32e2f01aa916724abef6cab6ae0
battosai52@hotmail.fr <LudovicDelmas@github.com>
2020-09-21 15:24:02 +0200

    rajout

---
+++ inscription.php
@@ -0,0 +1,13 @@
+<html>
+    <head><title>Formulaire d'inscription </title></head>
+    <body>
+        <h1>Inscrivez-vous !</h1>
+        <h2>Entrez les données demandées :</h2>
+        <form name="inscription" method="post" action="form.php">
+            <label for = "nom">Entrez votre nom : </label><input type="text" name="nom"/> <br/>
+            <label for = "email">Entrez votre email : </label><input type="text" name="email" /><br/>
+            <label for = "mot de passe">Entrez votre mot de passe : </label><input type="text" name="mot de passe"/><br/>
+            <input type="submit" name="valider" value="OK"/>
+        </form>
+    </body>
+</html>
