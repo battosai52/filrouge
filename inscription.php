7bd453d5a84a482ce05d6cfb72e33598d2368d81
battosai52@hotmail.fr <LudovicDelmas@github.com>
2020-09-30 11:42:19 +0200

    mise en page formulaire

--- inscription.php
+++ inscription.php
@@ -1,50 +1,66 @@
 <!doctype html>
 <html lang="fr">
-  <head>
-    
-    <meta charset="utf-8">
-       
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-<link rel="stylesheet" href="./style.css">
-<div class="banniere"><img src="./images/banniere.jpg"class="img-fluid" alt="Responsive image"></div>
-  </head>
-  <body>
- <div id="navbar">   
-<nav class="navbar navbar-expand-lg bg-primary">
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mx-auto">
-      <li class="nav-item">
-        <a class=" h1 nav-link text-danger"  href="index.php">Accueil </a>
-      </li>
-      <li class="nav-item">
-        <a class="h1 nav-link text-danger" href="attractions.html">Attractions</a>
-      </li>    
-        
-      <li class="nav-item">
-        <a class="h1 nav-link text-danger" href="reservation.php">Réservation</a>
-      </li>
- 	<li class="nav-item">
-        <a class="h1 nav-link text-danger" href="inscription.php">Inscription</a>
-      </li>
-    </ul>
-</div>  
- </nav>
+<head>
+
+	<meta charset="utf-8">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body class="bg-danger">
+	<div class="banniere row">
+		<img src="./images/banniere.jpg" class="mx-auto images" alt="Responsive image">
+	</div>
+	<div id="navbar">
+		<nav class="navbar navbar-expand-lg bg-primary">
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mx-auto">
+					<li class="nav-item">
+						<a class=" h1 nav-link text-danger" href="index.php">Accueil </a>
+					</li>
+					<li class="nav-item">
+						<a class="h1 nav-link text-danger" href="attractions.html">Attractions</a>
+					</li>
+
+					<li class="nav-item">
+						<a class="h1 nav-link text-danger" href="reservation.php">Réservation</a>
+					</li>
+					<li class="nav-item">
+						<a class="h1 nav-link text-danger" href="inscription.php">Inscription</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+<div class="container">
+	<div id="container">
+		<h1>Inscrivez-vous !</h1>
+		<h2>Entrez les données demandées :</h2>
+		<form name="inscription" method="post" action="processInscription.php">
+			<label for="nom">Entrez votre nom : </label><input type="text" name="nom" /> <br />
+			<label for="prénom">Entrez votre prénom : </label><input type="text" name="prénom" /> <br />
+			<label for="email">Entrez votre email : </label><input type="text" name="email" /><br />
+			<label for="mdp">Entrez votre mot de passe : </label><input type="text" name="mdp" /><br />
+			<label for="confmdp">Entrez a nouveau votre mot de passe : </label><input type="text" name="confmdp" /><br />
+			<input type="submit" name="valider" value="OK" />
+		</form>
+	</div>
 </div>
-<h1>Inscrivez-vous !</h1>
-        <h2>Entrez les données demandées :</h2>
-        <form name="inscription" method="post" action="processInscription.php">
-            <label for = "nom">Entrez votre nom : </label><input type="text" name="nom"/> <br/>
-            <label for = "prénom">Entrez votre prénom : </label><input type="text" name="prénom"/> <br/>
-            <label for = "email">Entrez votre email : </label><input type="text" name="email" /><br/>
-            <label for = "mdp">Entrez votre mot de passe : </label><input type="text" name="mdp"/><br/>
-            <label for = "confmdp">Entrez a nouveau votre mot de passe : </label><input type="text" name="confmdp"/><br/>
-            <input type="submit" name="valider" value="OK"/>
-        </form>
-        <?php
-        if(isset($erreur)) {
-           echo '<font color="red">'.$erreur."</font>";
-        }
-        ?>
-    </body>
+	<?php
+	if (isset($erreur)) {
+		echo '<font color="red">' . $erreur . "</font>";
+	}
+	?>
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+
 </html>
--- processInscription.php
+++ processInscription.php
@@ -1,40 +1,38 @@
 
 
 <?php
-	 $servername = 'localhost';
-     $username = 'root';
-     $password = '';
-     
-     //On s'inscrit
-     try{
-         $conn = new PDO("mysql:host=$servername;dbname=filrouge", $username, $password);
-         //On définit le mode d'erreur de PDO sur Exception
-         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-         
-     }
-     
-     /*On capture les exceptions si une exception est lancée et on affiche
-      *les informations relatives à celle-ci*/
-     catch(PDOException $e){
-       echo "Erreur : " . $e->getMessage();
-     }
+$servername = 'localhost';
+$username = 'root';
+$password = '';
 
-	if(isset($_POST['valider'])) {
-       $nom = htmlspecialchars($_POST['nom']);
-       $prénom = htmlspecialchars($_POST['prénom']);
-	   $email = htmlspecialchars($_POST['email']);
-	   $mdp = sha1($_POST['mdp']);
-	   $confmdp = sha1($_POST['confmdp']);
-	   if(!empty($_POST['nom']) AND !empty($_POST['prénom']) AND !empty($_POST['email']) AND !empty($_POST['mdp']) AND !empty($_POST['confmdp'])) {
-	      
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+//On s'inscrit
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=filrouge", $username, $password);
+    //On définit le mode d'erreur de PDO sur Exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+/*On capture les exceptions si une exception est lancée et on affiche
+      *les informations relatives à celle-ci*/ catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+
+if (isset($_POST['valider'])) {
+    $nom = htmlspecialchars($_POST['nom']);
+    $prénom = htmlspecialchars($_POST['prénom']);
+    $email = htmlspecialchars($_POST['email']);
+    $mdp = sha1($_POST['mdp']);
+    $confmdp = sha1($_POST['confmdp']);
+    if (!empty($_POST['nom']) and !empty($_POST['prénom']) and !empty($_POST['email']) and !empty($_POST['mdp']) and !empty($_POST['confmdp'])) {
+
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $reqmail = $conn->prepare("SELECT * FROM utilisateur WHERE email = ?");
             $reqmail->execute(array($email));
             $mailexist = $reqmail->rowCount();
-            if($mailexist == 0) {
-                if($mdp == $confmdp) {
+            if ($mailexist == 0) {
+                if ($mdp == $confmdp) {
                     $insertmbr = $conn->prepare("INSERT INTO utilisateur(nom, prénom, email, mdp) VALUES(?, ?, ?, ?)");
-                    $insertmbr->execute(array($nom,$prénom, $email, $mdp));
+                    $insertmbr->execute(array($nom, $prénom, $email, $mdp));
                     $erreur = "Votre compte a bien été créé ! <a href=\"connexion.php\">Me connecter</a>";
                 } else {
                     $erreur = "Vos mots de passes ne correspondent pas !";
@@ -45,10 +43,8 @@
         } else {
             $erreur = "Votre adresse mail n'est pas valide !";
         }
-        
-
-	   } else {
-	      $erreur = "Tous les champs doivent être complétés !";
-	   }
-	}
-	?>
+    } else {
+        $erreur = "Tous les champs doivent être complétés !";
+    }
+}
+?>
